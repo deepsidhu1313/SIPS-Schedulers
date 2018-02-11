c7cdf4955600a3e890c1dc76bcaa792501d9ee53
Navdeep Singh Sidhu <deepsidhu1313@users.noreply.github.com>
2018-02-11 16:15:30 -0600

    * Initial Experiment with scheduler
    Signed-off-by: Navdeep Singh Sidhu <deepsidhu1313@users.noreply.github.com>

--- src/main/java/in/co/s13/sips/schedulers/Chunk.java
+++ src/main/java/in/co/s13/sips/schedulers/Chunk.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2018 Navdeep Singh Sidhu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package in.co.s13.sips.schedulers;
 
@@ -10,20 +21,22 @@ import in.co.s13.sips.lib.TaskNodePair;
 import in.co.s13.sips.lib.common.datastructure.LiveNode;
 import in.co.s13.sips.scheduler.Scheduler;
 import java.util.ArrayList;
+import java.util.Collections;
 
-/**
- *
- * @author nika
- */
-public class Chunk implements Scheduler{
+public class Chunk implements Scheduler {
 
     @Override
     public ArrayList<TaskNodePair> schedule() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public ArrayList<ParallelForSENP> scheduleParallelFor(ArrayList<LiveNode> nodes,Object start,Object end,boolean reverseLoop,int dataType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @Override
+    public ArrayList<ParallelForSENP> scheduleParallelFor(ArrayList<LiveNode> nodes, Object start, Object end, boolean reverseLoop, int dataType) {
+        ArrayList<ParallelForSENP> result = new ArrayList<>();
+        System.out.println("Before Sorting:" + nodes);
+        Collections.sort(nodes, LiveNode.LiveNodeComparator.CPU_COMPOSITE_SCORE.thenComparing(LiveNode.LiveNodeComparator.DISTANCE_FROM_CURRENT));
+        System.out.println("After Sorting:" + nodes);
+        return result;
     }
-    
+
 }
